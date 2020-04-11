ef26d19792aa2406f9aef20918bcb4c0b600f2c7
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-04-11 18:54:13 +0000

    Added service card component

---
+++ static-site/src/components/service-card.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Img from 'gatsby-image';
+import {
+  Card,
+  CardTitle,
+  CardBody,
+  CardText
+} from 'reactstrap';
+
+const ServiceCard = props => (
+  <Card
+    className={`service-card${props.page === 'index' ? '-index' : ''} border rounded bg-white h-100`}>
+    <Img
+      className="rounded-top image-angle"
+      fluid={props.image} />
+    <CardBody>
+      <CardTitle>
+        <h3>
+          {props.title}
+        </h3>
+      </CardTitle>
+      <CardText>
+        {props.description}
+      </CardText>
+    </CardBody>
+  </Card>
+);
+
+export default ServiceCard;
