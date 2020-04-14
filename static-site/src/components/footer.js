2dde9333365a911d9bea95507259f29fcb999387
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-04-14 17:55:19 +0000

    Updated footer with hours

--- static-site/src/components/footer.js
+++ static-site/src/components/footer.js
@@ -15,75 +15,117 @@ const Footer = () => (
       <Row>
         <Col
           lg={4}
-          xs={6}>
-            <h5>
-              Sitemap
-            </h5>
-            <p
-              className="mb-0">
-              <Link
-                to="/">
-                Home
-              </Link>
-            </p>
-            <p
-              className="mb-0">
-              <Link
-                to="/about/">
-                About
-              </Link>
-            </p>
-            <p
-              className="mb-0">
-              <Link
-                to="/services/">
-                Services
-              </Link>
-            </p>
-            <p>
-              <Link
-                to="/contact/">
-                Contact
-              </Link>
-            </p>
-          </Col>
-          <Col
-          lg={4}
-          xs={6}>
-            <h5>
-              Address
-            </h5>
-            <p
-              className="mb-0">
-              Hi-Tech Auto
-            </p>
-            <p
-              className="mb-0">
-              4534 Baldwin Ave,
-            </p>
-            <p>
-               El Monte, CA 91731
-            </p>
-          </Col>
-          <Col
-          lg={4}
-          sm={6}
+          md={6}
           xs={12}>
+          <h5>
+            Sitemap
+          </h5>
+          <p
+            className="mb-0">
+            <Link
+              to="/">
+              Home
+            </Link>
+          </p>
+          <p
+            className="mb-0">
+            <Link
+              to="/about/">
+              About
+            </Link>
+          </p>
+          <p
+            className="mb-0">
+            <Link
+              to="/services/">
+              Services
+            </Link>
+          </p>
+          <p>
+            <Link
+              to="/contact/">
+              Contact
+            </Link>
+          </p>
+        </Col>
+        <Col
+          lg={4}
+          md={6}
+          xs={12}>
+          <h5>
+            Hours
+          </h5>
+          <div
+            className="mb-3">
             <div
-              className="bg-dark h-100 mb-3 d-flex">
-                <h6
-                  className="m-auto text-white">
-                    Map goes here
-                </h6>
+              className="d-flex justify-content-between mb-1">
+              <p
+                className="mb-0">Monday</p>
+              <p
+                className="mb-0">9:00AM - 6:00PM</p>
             </div>
-          </Col>
+            <div
+              className="d-flex justify-content-between mb-1">
+              <p
+                className="mb-0">Tuesday</p>
+              <p
+                className="mb-0">9:00AM - 6:00PM</p>
+            </div>
+            <div
+              className="d-flex justify-content-between mb-1">
+              <p
+                className="mb-0">Wednesday</p>
+              <p
+                className="mb-0">9:00AM - 6:00PM</p>
+            </div>
+            <div
+              className="d-flex justify-content-between mb-1">
+              <p
+                className="mb-0">Thursday</p>
+              <p
+                className="mb-0">Closed</p>
+            </div>
+            <div
+              className="d-flex justify-content-between mb-1">
+              <p
+                className="mb-0">Friday</p>
+              <p
+                className="mb-0">9:00AM - 6:00PM</p>
+            </div>
+            <div
+              className="d-flex justify-content-between mb-1">
+              <p
+                className="mb-0">Saturday</p>
+              <p
+                className="mb-0">9:00AM - 6:00PM</p>
+            </div>
+            <div
+              className="d-flex justify-content-between mb-1">
+              <p
+                className="mb-0">Sunday</p>
+              <p
+                className="mb-0">9:00AM - 6:00PM</p>
+            </div>
+          </div>
+        </Col>
+        <Col
+        lg={4}
+        xs={12}>
+          <div
+            className="bg-dark h-100 mb-3 d-flex">
+              <h6
+                className="m-auto text-white">
+                  Map goes here
+              </h6>
+          </div>
+        </Col>
         <Col
           xs={12}>
-            <p
-              className="text-center mt-1 mb-0">
-                Hi-Tech Auto © {new Date().getFullYear()}
-            </p>
-          </Col>
+          <p
+            className="text-center mt-3 mb-0">
+              Hi-Tech Auto © {new Date().getFullYear()}
+          </p>
+        </Col>
       </Row>
     </Container>
   </footer>
