d7f82629575878b890b75c1cc4831d8d54a7fb40
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2021-05-31 23:39:31 -0700

    Centered banner text

--- src/components/message-banner.js
+++ src/components/message-banner.js
@@ -11,7 +11,7 @@ const MessageBanner = () => {
       <Row>
         <Col xs={12}>
           <div className="d-flex justify-content-sm-center justify-content-start">
-            <small className="text-white text-md-center my-auto">
+            <small className="text-white text-center my-auto">
               Hi-Tech Auto has permanently closed its doors as of 05/31/21
             </small>
           </div>
